83d498d078b576225785f260af85440ecf1d79c9
Kaleb Kougl <kalebkougl@gmail.com>
2019-05-16 15:31:33 -0700

    adding deleteComment function & mock & add to yml

---
+++ finstagram-api/functions/comments/deleteComment.js
@@ -0,0 +1,21 @@
+import * as dynamoDbLib from "../../libs/dynamodb-lib";
+import { success, failure } from "../../libs/response-lib";
+
+export async function main(event, context) {
+    const data = JSON.parse(event.body);
+    const params = {
+        TableName: "photo_comments",
+        Key: {
+            photoId: event.pathParameters.id_photo,
+            commentId: data.id_comment
+        }
+    };
+
+    try {
+        const result = await dynamoDbLib.call("update", params);
+        return success({ status: true });
+    } catch (e) {
+        console.log(e);
+        return failure({ status: false });
+    }
+}
