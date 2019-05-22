90aa6d795865d25a01f6c7b1fe1da3f796fa19a0
Kaleb Kougl <kalebkougl@gmail.com>
2019-05-22 10:57:17 -0700

    adding a form with validation, some styling to make comments on pictures, needs to be hooked up to backend

--- client/src/containers/SinglePhoto.js
+++ client/src/containers/SinglePhoto.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Button } from 'react-bootstrap';
+import { Button, Form } from 'react-bootstrap';
 import { Link } from '@reach/router';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import { faArrowLeft, faCommentAlt } from '@fortawesome/free-solid-svg-icons';
 import { attemptAWSLoadPhotoComments, clearPhotoDetails } from '../actions';
 import './styles/SinglePhoto.css';
 
-library.add(faArrowLeft);
+library.add(faArrowLeft, faCommentAlt);
 
 class SinglePhoto extends Component {
     constructor(props) {
@@ -18,16 +18,8 @@ class SinglePhoto extends Component {
             likes: 0,
             description: null,
             img: null,
-        }
-    }
-
-    grabSingularPhoto = (id) => {
-        let viewingPhoto;
-        if (this.props.photos !== null) {
-            viewingPhoto = this.props.photos.filter(photo => photo.photoId === this.props.photoId);
-            this.setState({ img: viewingPhoto[0] || null });
-        } else {
-            // dispatch action to get the photo
+            commentFormVisible: false,
+            newComment: '',
         }
     }
 
@@ -39,6 +31,34 @@ class SinglePhoto extends Component {
     componentDidUpdate(prevProps, prevState) {
         if (prevProps.comments === null && this.props.comments !== null) {
             this.setState({ comments: this.props.comments });
+        }
+    }
+
+    componentWillUnmount() {
+        this.props.clearPhotoDetails();
+    }
+
+    handleClick = () => {
+        this.setState({ commentFormVisible: true });
+    }
+
+    handleChange = event => {
+        this.setState({
+            [event.target.id]: event.target.value
+        });
+    }
+
+    validateComment = () => {
+        return this.state.newComment.length > 0 && this.state.newComment.length < 120;
+    }
+
+    grabSingularPhoto = (id) => {
+        let viewingPhoto;
+        if (this.props.photos !== null) {
+            viewingPhoto = this.props.photos.filter(photo => photo.photoId === this.props.photoId);
+            this.setState({ img: viewingPhoto[0] || null });
+        } else {
+            // dispatch action to get the photo
         }
     }
 
@@ -54,8 +74,27 @@ class SinglePhoto extends Component {
         return comments;
     }
 
-    componentWillUnmount() {
-        this.props.clearPhotoDetails();
+    renderCommentForm = () => {
+        return (
+            <Form className="comment-form">
+                <Form.Group controlId="newComment">
+                    <Form.Label>Write your comment here!</Form.Label>
+                    <Form.Control
+                        as="textarea"
+                        rows="3"
+                        value={this.state.newComment}
+                        onChange={this.handleChange}
+                    />
+                </Form.Group>
+                <Button
+                    disabled={!this.validateComment()}
+                    variant="light"
+                    type="submit"
+                >
+                    Submit!
+                </Button>
+            </Form>
+        );
     }
 
     render() {
@@ -66,10 +105,16 @@ class SinglePhoto extends Component {
                         <FontAwesomeIcon icon="arrow-left" className="backArrow" />
                     </Button>
                 </Link>
-                {this.state.img ? <img src={this.state.img.photo} alt={this.state.img.description} className="singlePhoto" /> : <div>no image found</div>}
-                {this.state.img ? <p className="description">{this.state.img.description}</p> : <p>Loading...</p>}
+                {this.state.img ? <img src={this.state.img.photo} alt={this.state.img.description} className="singlePhoto" /> : <p>No image found.</p>}
+                <article className="photo-details">
+                    {this.state.img ? <p className="description">{this.state.img.description}</p> : <p className="description">Loading...</p>}
+                    <Button variant="light" onClick={this.handleClick} className="comment-btn">
+                        <FontAwesomeIcon icon="comment-alt" className="comment-icon" />
+                    </Button>
+                </article>
                 <article className="comments-container">
                     {this.state.comments !== null ? this.renderComments() : <div>Loading comments...</div>}
+                    {this.state.commentFormVisible && this.renderCommentForm()}
                 </article>
             </div>
         )
